6561ac7a123ee89e67c46c0f2efb8150a599af17
Michael Kong <michael@scalapay.com>
2023-08-02 23:50:14 +1000

    add api helpers

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+import Button from '@/components/Button';
+import React from 'react';
+
+const Register = () => {
+  return (
+    <div>
+      <Button intent="secondary">Register</Button>
+    </div>
+  );
+};
+
+export default Register;
