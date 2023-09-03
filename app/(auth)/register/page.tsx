1ff2c1e8f77f8d5b8318d9e16a50877b7981474e
Michael Kong <michael@vivodigital.com.au>
2023-09-03 22:41:17 +1000

    UI: update AuthForm component with styles and content

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -4,9 +4,7 @@ import AuthForm from '@/components/AuthForm';
 import Button from '@/components/Button';
 
 const Register = () => {
-  return (
-    <AuthForm type='register'/>
-  );
+  return <AuthForm mode="register" />;
 };
 
 export default Register;
--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,12 +1,9 @@
 'use client';
 import React from 'react';
 import AuthForm from '@/components/AuthForm';
-import Button from '@/components/Button';
 
 const SignIn = () => {
-  return (
-    <AuthForm type='signin'/>
-  );
+  return <AuthForm mode="signin" />;
 };
 
 export default SignIn;
